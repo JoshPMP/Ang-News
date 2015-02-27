ebb849eb13c3acd94c0b9bef80c60ec69fea950b
JoshPMP <joshua@propertymanagementpros.com>
2015-02-27 09:18:36 -0500

    add login and register changes
    Everything is actually working right now.  I can login, register, and
    logout successfully.  There is an auth issue when clicking on the
    ng-news logo it allows you to see posts w/o logging in

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -1,31 +1,28 @@
+
 'use strict';
 
-app.controller('AuthCtrl', ['$scope', '$location', 'Auth', 'user', '$firebase', function($scope, $location, $firebase, Auth, user){
-	if (user) {
-		$location.path('/');  		//If the user exists, send them to the home page
-	}
+app.controller('AuthCtrl', function ($scope, $location, Auth, user) {
+  if (user) {
+    $location.path('/');
+  }
 
-
-
-	  $scope.login = function () {
-	    Auth.login($scope.user).then(function () {
+    $scope.login = function () {
+	   Auth.login($scope.user).then(function () {
 	      $location.path('/');
 	    }, function (error) {
 	      $scope.error = error.toString();
 	    });
 	  };
 
-	    $scope.register = function () {
-		    Auth.register($scope.user).then(function() {         
-		      return Auth.login($scope.user).then(function() {   
-		        $location.path('/');							 
-		      });
-		    }, function(error) {					
-		      $scope.error = error.toString();					
-		    });
-		  };
-
-
-}]);
+  $scope.register = function () {
+    Auth.register($scope.user).then(function() {
+      return Auth.login($scope.user).then(function() {
+        $location.path('/');
+      });
+    }, function(error) {
+      $scope.error = error.toString();
+    });
+  };
+});
 
 
